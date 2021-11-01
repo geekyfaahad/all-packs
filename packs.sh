15049f9167445489dff52168730e720592322962
Faahad <77067677+geekyfaahad@users.noreply.github.com>
2021-11-01 09:26:06 -0700

    Create packs.sh

---
+++ packs.sh
@@ -0,0 +1,27 @@
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get install -y libcurl4-openssl-dev
+sudo apt-get install -y libssl-dev
+sudo apt-get install -y jq
+sudo apt-get install -y ruby-full
+sudo apt-get install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev build-essential libgmp-dev zlib1g-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install -y python-setuptools
+sudo apt-get install -y libldns-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dnspython
+sudo apt-get install -y git
+sudo apt-get install -y rename
+sudo apt-get install -y xargs
+sudo apt install imview
+sudo apt install net-tools
+sudo apt install eog
+sudo apt install terminology
+sudo apt install gzip
+sudo apt install ruby
+sudo apt install curl
+sudo apt install imview
+sudo apt install python2
+sudo apt install python3
+sudo apt install python3-pip
